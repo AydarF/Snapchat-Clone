19ade84e921c1f0b6d1ca61f5d74b534d9a713bf
AydarF <aydar_fayzullin@yahoo.com>
2021-01-14 19:18:23 -0500

    Display a snapshot, redirect to home page when there's no picture and redirect to home page on close icon click

--- src/Preview.js
+++ src/Preview.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { resetCameraImage, selectCameraImage } from "./features/cameraSlice";
+import CloseIcon from "@material-ui/icons/Close";
 import "./Preview.css";
 
 function Preview() {
-  return <div className="preview"></div>;
+  const cameraImage = useSelector(selectCameraImage);
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (!cameraImage) {
+      history.replace("/");
+    }
+  }, [cameraImage, history]);
+
+  const closePreview = () => {
+    dispatch(resetCameraImage());
+  };
+
+  return (
+    <div className="preview">
+      <CloseIcon onClick={closePreview} className="preview__close" />
+      <img src={cameraImage} alt="" />
+    </div>
+  );
 }
 
 export default Preview;
