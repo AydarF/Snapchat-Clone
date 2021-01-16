bf20fd66efba0015faf1f8036857c0674342b813
AydarF <aydar_fayzullin@yahoo.com>
2021-01-16 15:08:01 -0500

    Create Preview toolbar a style it

--- src/Preview.js
+++ src/Preview.js
@@ -1,9 +1,16 @@
 import React, { useEffect } from "react";
+import "./Preview.css";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { resetCameraImage, selectCameraImage } from "./features/cameraSlice";
 import CloseIcon from "@material-ui/icons/Close";
-import "./Preview.css";
+import TextFieldsIcon from "@material-ui/icons/TextFields";
+import CreateIcon from "@material-ui/icons/Create";
+import NoteIcon from "@material-ui/icons/Note";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
+import CropIcon from "@material-ui/icons/Crop";
+import TimerIcon from "@material-ui/icons/Timer";
 
 function Preview() {
   const cameraImage = useSelector(selectCameraImage);
@@ -23,6 +30,15 @@ function Preview() {
   return (
     <div className="preview">
       <CloseIcon onClick={closePreview} className="preview__close" />
+      <div className="preview__toolbarRight">
+        <TextFieldsIcon />
+        <CreateIcon />
+        <NoteIcon />
+        <MusicNoteIcon />
+        <AttachFileIcon />
+        <CropIcon />
+        <TimerIcon />
+      </div>
       <img src={cameraImage} alt="" />
     </div>
   );
