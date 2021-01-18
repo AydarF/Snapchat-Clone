d4f156a8a56131bce7cb101d1b488f2a36585d01
AydarF <aydar_fayzullin@yahoo.com>
2021-01-18 01:44:33 -0500

    Create Login and add Google authentication

--- src/App.js
+++ src/App.js
@@ -1,31 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import WebcamCapture from "./WebcamCapture";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Preview from "./Preview";
 import Chats from "./Chats";
 import ChatView from "./ChatView";
+import { useSelector, useDispatch } from "react-redux";
+import { login, logout, selectUser } from "./features/appSlice";
+import Login from "./Login";
+import { auth } from "./firebase";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch(
+          login({
+            username: authUser.displayName,
+            profilePic: authUser.photoUrl,
+            id: authUser.uid,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
   return (
     <div className="app">
       <Router>
-        <div className="app__body">
-          <Switch>
-            <Route exact path="/">
-              <WebcamCapture />
-            </Route>
-            <Route path="/preview">
-              <Preview />
-            </Route>
-            <Route path="/chats/view">
-              <ChatView />
-            </Route>
-            <Route path="/chats">
-              <Chats />
-            </Route>
-          </Switch>
-        </div>
+        {!user ? (
+          <Login />
+        ) : (
+          <div className="app__body">
+            <Switch>
+              <Route exact path="/">
+                <WebcamCapture />
+              </Route>
+              <Route path="/preview">
+                <Preview />
+              </Route>
+              <Route path="/chats/view">
+                <ChatView />
+              </Route>
+              <Route path="/chats">
+                <Chats />
+              </Route>
+            </Switch>
+          </div>
+        )}
       </Router>
     </div>
   );
