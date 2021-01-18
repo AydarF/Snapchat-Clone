e6ba5010aff98a2d877697ab035dea88bdaa3489
AydarF <aydar_fayzullin@yahoo.com>
2021-01-17 20:29:10 -0500

    Create and style all Chats section

---
+++ src/Chats.js
@@ -0,0 +1,40 @@
+import { Avatar } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import "./Chats.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
+import { db } from "./firebase";
+
+function Chats() {
+  const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    db.collection("posts")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
+  }, []);
+
+  return (
+    <div className="chats">
+      <div className="chats__header">
+        <Avatar className="chats__avatar" />
+        <div className="chats__search">
+          <SearchIcon />
+          <input placeholder="Friends" type="text" />
+        </div>
+        <ChatBubbleIcon className="chats__chatIcon" />
+      </div>
+
+      <div className="chats__posts"></div>
+    </div>
+  );
+}
+
+export default Chats;
